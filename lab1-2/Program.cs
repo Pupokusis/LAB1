3f54205f3360656420599a2455bb4319531c2d5d
Igor <igor.dergulev777@yandex.ru>
2025-03-04 15:23:14 +0800

    Код для задачи for

---
+++ lab1-2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число x: ");
+        int x = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите число y: ");
+        int y = int.Parse(Console.ReadLine());
+
+        if (x <= 0 || y <= 0)
+        {
+            Console.WriteLine("Ошибка: оба числа должны быть натуральными (больше 0).");
+            return;
+        }
+
+        int result = MultiplyUsingAddition(x, y);
+
+        Console.WriteLine($"Произведение {x} и {y} равно {result}");
+    }
+
+    static int MultiplyUsingAddition(int x, int y)
+    {
+        int result = 0;
+
+        for (int i = 0; i < y; i++)
+        {
+            result += x;
+        }
+
+        return result;
+    }
+}
